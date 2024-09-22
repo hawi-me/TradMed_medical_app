fb0809e203e004c679fc0288fe2a84d46a3f122e
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2024-09-23 02:16:30 +0300

    Add files via upload
    backend structure

---
+++ backend_GOLANG/domain/structure.go
@@ -0,0 +1,30 @@
+package domain
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Disease struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Name        string             `bson:"name"`
+	Description string             `bson:"description"`
+	Symptoms    []string           `bson:"symptoms"`
+	Treatment   string             `bson:"treatment"`
+	Prevention  string             `bson:"prevention"`
+}
+
+type Nutrient struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Name        string             `bson:"name"`
+	Description string             `bson:"description"`
+	Deficiency  string             `bson:"deficiency"`
+	Source      string             `bson:"source"`
+}
+
+type Herb struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Name        string             `bson:"name"`
+	Description string             `bson:"description"`
+	Usage       string             `bson:"usage"`
+	Preparation string             `bson:"preparation"`
+}
